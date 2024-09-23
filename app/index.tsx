7d57e300f79d5fdfc2e653935e44c99ee50822b1
Htoomyat99 <htoomyat.20399@gmail.com>
2024-09-23 23:28:40 +0630

    add images , add packages

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import Icon from "@/assets/images/wordle-icon.svg";
 
 export default function Index() {
   return (
@@ -9,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Icon width={100} height={100} />
       <Text>Index</Text>
     </View>
   );
