3786e74505933995ac858a6c6f0a750e133093a1
Htoomyat99 <htoomyat.20399@gmail.com>
2024-09-24 23:11:06 +0630

    size-matter added, signUP UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,33 @@
 import { Stack } from "expo-router";
+import {
+  useFonts,
+  FrankRuhlLibre_800ExtraBold,
+  FrankRuhlLibre_500Medium,
+  FrankRuhlLibre_900Black,
+} from "@expo-google-fonts/frank-ruhl-libre";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    FrankRuhlLibre_800ExtraBold,
+    FrankRuhlLibre_500Medium,
+    FrankRuhlLibre_900Black,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,99 @@
-import { Text, View } from "react-native";
 import Icon from "@/assets/images/wordle-icon.svg";
+import { Link } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { moderateScale, scale, verticalScale } from "react-native-size-matters";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Icon width={100} height={100} />
-      <Text>Index</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Icon width={scale(100)} height={verticalScale(70)} />
+
+        <Text style={styles.title}>Worlde</Text>
+
+        <Text style={styles.text}>Get 6 chances to guess a 5-letter word.</Text>
+      </View>
+
+      <View style={styles.menu}>
+        <Link
+          href={"/game"}
+          asChild
+          style={[styles.btn, { backgroundColor: "#000" }]}
+        >
+          <Pressable>
+            <Text style={[styles.btnText, styles.primaryText]}>Play</Text>
+          </Pressable>
+        </Link>
+
+        <Pressable style={styles.btn}>
+          <Text style={styles.btnText}>Play</Text>
+        </Pressable>
+
+        <Pressable>
+          <Text style={[styles.btnText, styles.primaryText]}>Log in</Text>
+        </Pressable>
+
+        <Pressable style={styles.btnText}>
+          <Text style={[styles.btnText, styles.primaryText]}>Subscribe</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.text}>Made by simon</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: scale(50),
+    gap: verticalScale(40),
+  },
+  header: {
+    alignItems: "center",
+    gap: verticalScale(10),
+  },
+  title: {
+    fontSize: moderateScale(40),
+    fontFamily: "FrankRuhlLibre_800ExtraBold",
+  },
+  text: {
+    fontSize: moderateScale(26),
+    textAlign: "center",
+    fontFamily: "FrankRuhlLibre_500Medium",
+  },
+  menu: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: verticalScale(10),
+  },
+  footer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: moderateScale(30),
+    borderColor: "#000",
+    borderWidth: 1,
+    width: "60%",
+    maxWidth: scale(200),
+  },
+  btnText: {
+    paddingHorizontal: scale(14),
+    paddingVertical: verticalScale(10),
+    fontSize: moderateScale(16),
+    color: "#333333",
+    fontFamily: "FrankRuhlLibre_500Medium",
+  },
+  primaryItem: {
+    backgroundColor: "#000",
+  },
+  primaryText: {
+    color: "#FFF",
+  },
+});
